d98a5a3677ce38c68a21ce16aaf4e755499b01d6
Teoman Kirac <teomankirac@gmail.com>
2016-11-25 14:24:13 +0100

    Update keystone.js

--- keystone.js
+++ keystone.js
@@ -1,13 +1,7 @@
-// Simulate config options from your production environment by
-// customising the .env file in your project's root folder.
 require('dotenv').config();
 
 // Require keystone
 var keystone = require('keystone');
-
-// Initialise Keystone with your project's configuration.
-// See http://keystonejs.com/guide/config for available options
-// and documentation.
 
 keystone.init({
 	'name': 'trioduo',
@@ -22,8 +16,6 @@ keystone.init({
 	'emails': 'templates/emails',
 
 	'auto update': true,
-	//'mongo': 'mongodb://localhost/local',
-	//'mongo': 'mongodb://trioduo',
 	'session': true,
 	'auth': true,
 	'user model': 'User',
@@ -45,9 +37,6 @@ keystone.init({
 // Load your project's Models
 keystone.import('models');
 
-// Setup common locals for your templates. The following are required for the
-// bundled templates and layouts. Any runtime locals (that should be set uniquely
-// for each request) should be added to ./routes/middleware.js
 keystone.set('locals', {
 	_: require('lodash'),
 	env: keystone.get('env'),
@@ -82,7 +71,6 @@ keystone.set('email tests', require('./routes/emails'));
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
 	nodes: ['nodes', 'edges', 'node-comments'],
-	//galleries: 'galleries',
 	users: 'users',
 });
 
