a2907a952e8e974f50f04a7c8de49ffedd35b1ca
Teoman Kirac <teomankirac@gmail.com>
2016-11-25 14:34:20 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,3 @@
-/**
- * This file is where you define your application routes and controllers.
- *
- * Start by including the middleware you want to run for every request;
- * you can attach middleware to the pre('routes') and pre('render') events.
- *
- * For simplicity, the default setup for route controllers is for each to be
- * in its own file, and we import all the files in the /routes/views directory.
- *
- * Each of these files is a route controller, and is responsible for all the
- * processing that needs to happen for the route (e.g. loading data, handling
- * form submissions, rendering the view template, etc).
- *
- * Bind each route pattern your application should respond to in the function
- * that is exported from this module, following the examples below.
- *
- * See the Express application routing documentation for more information:
- * http://expressjs.com/api.html#app.VERB
- */
-
 var keystone = require('keystone');
 var babelify = require('babelify');
 var bodyParser = require('body-parser');
@@ -60,13 +40,14 @@ exports = module.exports = function (app) {
 	app.get('/', routes.views.index);
 	app.get('/add');
 	app.get('/add/node/:node');
+	
+	// To get JSON endpoints for cytoscape
 
 	// Api for
 	app.get('/api/edge/list', keystone.middleware.api, routes.api.edges.list);
 	app.get('/api/node/list', keystone.middleware.api, routes.api.nodes.list);
+	
+	//upload CSV and populate database option
 	//app.post('/uploadcsv_post', keystone.middleware.cors, routes.api.uploadcsv);
-
-	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
-	// app.get('/protected', middleware.requireUser, routes.views.protected);
 
 };
